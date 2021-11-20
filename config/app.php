288c47248bae3e2f42dd6ab53036a2402ab199e6
royhan76 <beensholih@gmail.com>
2021-11-20 15:03:19 +0700

    ganti localhost

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://roichandev.skom.id/wedding/'),
 
     'asset_url' => env('ASSET_URL', null),
 
